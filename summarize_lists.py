80f0c00f8c9a6055249979504027ddf019e226ca
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-08-31 16:59:45 +0530

    Update summarize_lists.py

--- summarize_lists.py
+++ summarize_lists.py
@@ -2,7 +2,7 @@ __author__ = 'linda-ge'
 import csv
 from requests import get
 from requests.exceptions import RequestException
-from contextlib import closing     
+from contextlib import closing    
 from bs4 import BeautifulSoup     
 def simple_get(url):
     """ 
